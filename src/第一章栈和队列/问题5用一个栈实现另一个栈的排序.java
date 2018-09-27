e0ab80007727c3d9a5fe1ed50061def44a25fec9
XZZMemory <1933850821@qq.com>
2018-09-27 20:37:24 +0800

    2018.9.27

--- src/第一章栈和队列/问题5用一个栈实现另一个栈的排序.java
+++ src/第一章栈和队列/问题5用一个栈实现另一个栈的排序.java
@@ -6,10 +6,30 @@ import java.util.Stack;
  * created by memory
  * on 2018/9/27 下午4:06
  */
+/*
+ * 将栈从顶到底按照从大到小的顺序排序，只许申请一个栈，初次之外，可以申请新的变量，但不能申请额外的数据结构，如何完成排序
+ */
 public class 问题5用一个栈实现另一个栈的排序 {
-    public void sortStackByStack(Stack<Integer> stack)
+    public static void main(String[] args)
     {
-
+        Stack<Integer> stack=new Stack<>();
+        int[] data={3,1,2,4};
+        for (int i=0;i<data.length;i++)
+            stack.push(data[i]);
+        sortStackByStack(stack);
+    }
+    public static void sortStackByStack(Stack<Integer> stack)
+    {
+        Stack<Integer> help=new Stack<>();
+        while (!stack.isEmpty())
+        {
+            int currentData=stack.pop();
+            while(!help.isEmpty()&&help.peek()<currentData)
+                stack.push(help.pop());
+            help.push(currentData);
+        }
+        while (!help.isEmpty())
+            stack.push(help.pop());
     }
 }
 
